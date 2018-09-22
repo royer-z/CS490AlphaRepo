4e628fee2e11a613ceb2530551c3daac68fba4c2
Royer <rvz2@njit.edu>
2018-09-22 15:23:28 -0700

    created a form

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
 
-echo("First PHP script!");
-
 ?>
 
 <!doctype html>
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Log-in (Alpha)</title>
+	<title>Login Screen (Alpha)</title>
 </head>
 
 <body>
+	<form>
+		UCID:<br>
+		<input  type="text" name="username"><br>
+		Password:<br>
+		<input type="password" name="password"><br><br>
+		<input type="submit" value="Login"
+	</form>
 </body>
 </html>
