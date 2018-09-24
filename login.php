51a658e3533dfd036ff2b2371ebd21927c52d36f
Royer <rvz2@njit.edu>
2018-09-23 20:12:08 -0700

    fetch try 1

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+// cURL in PHP
+
+/*
+$formData = $_POST["userData"];
+$cSession = curl_init(); // Send data using cURL
+curl_setopt($cSession, CURLOPT_URL, "https://web.njit.edu/~tmd24/CS490/checkAndPass.php");
+curl_setopt($cSession, CURLOPT_POST, TRUE);
+curl_setopt($cSession, CURLOPT_POSTFIELDS, $formData);
+curl_setopt($cSession, CURLOPT_RETURNTRANSFER, TRUE);
+$cResult = curl_exec($cSession);
+$curl_close($cSession);
+*/
+echo "It works";
+?>
